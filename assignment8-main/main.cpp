02ab2f5e623c8641f9f58793b5decfa7a7cf8abb
sawyersieja <sawyersieja@protonmail.com>
2023-11-21 23:18:41 -0800

    Added main.cpp as basic starting point.

---
+++ assignment8-main/main.cpp
@@ -0,0 +1,17 @@
+/*
+* main.cpp
+* Authors: Toan Nguyen, Sawyer Sieja
+* CS311
+* Assignment 8
+* Description: 
+*   This program will find the shortest route between cities, using the graph data structures.
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
